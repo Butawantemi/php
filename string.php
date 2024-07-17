a9291ba4e228408f20613c9233cc12ed91369cbb
Butawantemi <japhetbuta@gmail.com>
2024-07-17 19:16:58 +0300

    string.php

--- string.php
+++ string.php
@@ -1,7 +1,21 @@
 <?php
-$name = "Japhet";
+$intro = "hello world";
+//convert string to array
+$introArray = explode(" ", $intro);
+print_r($introArray);
+
+
+//change small letter to capital letter
+//echo strtoupper($intro);
+/* $name = "Japhet";
 
 echo "Hello $name!";
 
 //check length of the string 
-echo strlen($name);
+echo strlen($name); */
+
+//count word in string
+//echo str_word_count("Hello World!");
+
+//search for text within the string
+//echo strpos("Hello World", "World");
