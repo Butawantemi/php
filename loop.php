56402b3d2f1e4f4368c05799aa5bf23667d964c9
Butawantemi <japhetbuta@gmail.com>
2024-07-19 20:48:41 +0300

    loop

---
+++ loop.php
@@ -0,0 +1,41 @@
+<?php
+$i = 1;
+
+do {
+  echo $i;
+  $i++;
+} while ($i < 10);
+
+
+
+
+
+//while loop
+/* $i = 0;
+while ($i < 6) {
+  $i++;
+  if ($i == 3) continue;
+  echo $i;
+}
+ */
+
+
+
+/* //break statement
+$i = 1;
+while ($i < 6) {
+  if ($i == 3) break;
+  echo $i;
+  $i++;
+} */
+
+
+
+
+
+/* $i = 2;
+
+while($i <= 10){
+    echo $i."</br>";
+    $i++;
+} */
