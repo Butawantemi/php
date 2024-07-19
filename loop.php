9ca3d0d93c7ce904f6b30308492911f55abe5759
Butawantemi <japhetbuta@gmail.com>
2024-07-19 21:09:28 +0300

    loop

--- loop.php
+++ loop.php
@@ -1,15 +1,48 @@
 <?php
-$i = 1;
+class Car {
+  public $color;
+  public $model;
+  public function __construct($color, $model) {
+    $this->color = $color;
+    $this->model = $model;
+  }
+}
+
+$myCar = new Car("red", "Volvo");
+
+foreach ($myCar as $x => $y) {
+  echo "$x: $y \n";
+}
+
+
+
+//foreach loop
+/* $members = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+
+foreach ($members as $x => $y) {
+  echo "$x : $y \n";
+} */
+
+/* $colors = array("red", "green", "blue", "yellow");
+
+foreach ($colors as $value){
+   echo "$value \n";
+}
+ */
+
+//for loop
+/* for ($x = 0; $x <= 10; $x++) {
+  echo $x;
+}
+ */
+
+//Do while loop
+/* $i = 1;
 
 do {
   echo $i;
   $i++;
-} while ($i < 10);
-
-
-
-
-
+} while ($i < 10); */
 //while loop
 /* $i = 0;
 while ($i < 6) {
