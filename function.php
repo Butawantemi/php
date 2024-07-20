43532752b9dbb2ab2091374463f4c71015e7a9e7
Butawantemi <japhetbuta@gmail.com>
2024-07-20 10:11:34 +0300

    Array

---
+++ function.php
@@ -0,0 +1,38 @@
+<?php
+// sum of number in array
+function sumArray(...$arg){
+    $len = count($arg);
+    $sum = 0;
+    for ($i = 0; $i < $len; $i++){
+        $sum += $arg[$i];
+    }
+    return $sum;
+}
+
+echo sumArray(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+
+/* // passing arg by reference
+function addNum(&$value) {
+    $value += 4;
+}
+
+$num = 10;
+addNum($num);
+echo $num; */
+
+
+/* function sum($a = 9, $b=3){
+    $z = $a + $b;
+    return $z;
+}
+
+echo sum(2);
+ */
+/* function Greeting ($name = "Butawantemi"){
+    echo "Hello, $name! \n";
+}
+
+Greeting("Japhet Paul");
+Greeting();
+ */
