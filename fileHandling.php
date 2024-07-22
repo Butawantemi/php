060688f3669bf8447786e378e2adab2e8978b893
Butawantemi <japhetbuta@gmail.com>
2024-07-22 21:09:01 +0300

    file handling

--- fileHandling.php
+++ fileHandling.php
@@ -1,3 +1,10 @@
 <?php
-//readfile() function is used to read a file and write it to the output buffer.
-echo readfile("file.txt");
+//PHP File Handling
+//fopen() function is used to open a file.
+$file = fopen("file.txt", "r") or die("Unable to open file!");
+echo fread($file, filesize("file.txt"));
+fclose($file);
+
+
+/* //readfile() function is used to read a file and write it to the output buffer.
+echo readfile("file.txt"); */
