b28b3686b8fb03be75beaa6f4baf98e1b463a103
Butawantemi <japhetbuta@gmail.com>
2024-07-24 20:11:06 +0300

    class

--- OOP/class.php
+++ OOP/class.php
@@ -8,18 +8,18 @@ class fruit {
         $this->name = $name;
         $this->color = $color;
     }
-    function get_name(){
+  /*   function get_name(){
         return $this->name;
     }
     function get_color(){
         return $this->color;
-    }
+    } */
 }
 
 $apple = new fruit('Apple', 'Red');
 $banana = new fruit('Banana', 'Yellow');
 
-echo $apple->get_name()."\n";
-echo $apple->get_color()."\n";
-echo $banana->get_name()."\n";
-echo $banana->get_color()."\n";
+//echo $apple->get_name()."\n";
+//echo $apple->get_color()."\n";
+//echo $banana->get_name()."\n";
+//echo $banana->get_color()."\n";
