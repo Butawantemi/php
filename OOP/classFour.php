8ad13da9f15fe2a4a81497362e2ef8c0cdabe345
Butawantemi <japhetbuta@gmail.com>
2024-07-25 17:38:54 +0300

    access const variable

--- OOP/classFour.php
+++ OOP/classFour.php
@@ -2,7 +2,13 @@
 class Goodbye {
     //const variable
     const LEAVING_MESSAGE = "Thank you for visiting W3schools.com!";
+    //Aceessing using self keyword
+    public function byebye(){
+        echo self::LEAVING_MESSAGE;
+    }
 
 }
 //accessing const variable
-echo Goodbye::LEAVING_MESSAGE;
+//echo Goodbye::LEAVING_MESSAGE;
+$goodbye = new Goodbye();
+echo $goodbye->byebye();
