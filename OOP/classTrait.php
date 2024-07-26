ffe62d22a049acd1db3a1b550b087deb6808b0d9
Butawantemi <japhetbuta@gmail.com>
2024-07-26 21:21:19 +0300

    trait

--- OOP/classTrait.php
+++ OOP/classTrait.php
@@ -1,6 +1,6 @@
 <?php
 //Trait is mechanism to reuse the code in single inheritance.
-trait message {
+trait message1 {
     public function msg (){
         echo "OOP is fun! \n";
     }
@@ -14,12 +14,15 @@ trait message2 {
   }
 
 //class Welcome is using message trait
-class Welcome  {
-    use message;
+class Welcome1  {
+    use message1;
 }
 
-//class
+//class Welcome2 is using message and message2 trait
+class Welcome2 {
+    use message1, message2;
+  }
 
 //create object of Welcome class
-$obj = new Welcome();
+$obj = new Welcome1();
 $obj->msg();
