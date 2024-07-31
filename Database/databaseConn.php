35c10a25d10649d145b68ec937c4c36afcb5621b
Butawantemi <japhetbuta@gmail.com>
2024-07-31 21:08:38 +0300

    database connection

--- Database/databaseConn.php
+++ Database/databaseConn.php
@@ -7,7 +7,7 @@ $database = "firstdb";
 
 // Create connection
 $conn = new mysqli($server, $username, $password, $database);
-
+// Check connection
 if ($conn->connect_error) {
     die("Connection failed:" . $conn->connect_error);
 }
