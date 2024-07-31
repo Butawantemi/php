cdacb1c6458ed2e0ec084021208873eb6639eee4
Butawantemi <japhetbuta@gmail.com>
2024-07-31 20:12:22 +0300

    iterable

--- OOP/Iterable2.php
+++ OOP/Iterable2.php
@@ -1,17 +1,43 @@
 <?php
-class myIterable implements Iterator {
+// Create an Iterator
+class MyIterator implements Iterator {
   private $items = [];
   private $pointer = 0;
 
-  public function __construct($items){
+  public function __construct($items) {
+    // array_values() makes sure that the keys are numbers
     $this->items = array_values($items);
+  }
+
+  public function current() {
+    return $this->items[$this->pointer];
+  }
+
+  public function key() {
+    return $this->pointer;
+  }
+
+  public function next() {
+    $this->pointer++;
+  }
+
+  public function rewind() {
+    $this->pointer = 0;
+  }
+
+  public function valid() {
+    // count() indicates how many items are in the list
+    return $this->pointer < count($this->items);
   }
 }
 
-//function to return the current element
- public function current(){
-  return $this->items[$this->pointer];
+// A function that uses iterables
+function printIterable(iterable $myIterable) {
+  foreach($myIterable as $item) {
+    echo $item;
+  }
 }
+
 
 
 
