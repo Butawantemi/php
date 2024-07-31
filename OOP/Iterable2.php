f218f261b3d00c5780283ee10c74ffd41cd15e62
Butawantemi <japhetbuta@gmail.com>
2024-07-31 19:51:10 +0300

    iterable

--- OOP/Iterable2.php
+++ OOP/Iterable2.php
@@ -1,10 +1,12 @@
 <?php
-//function
+//function which returns an array
 function getIterable():iterable {
   return ["a", "b", "c"];
 }
 
+//use of iterable
 $myIterable = getIterable();
+//looping
 foreach($myIterable as $item) {
   echo $item;
 }
