b7aa2c7f8256d4a5dcd9a160b147594d41042420
Butawantemi <japhetbuta@gmail.com>
2024-07-31 20:05:07 +0300

    iterable

--- OOP/Iterable2.php
+++ OOP/Iterable2.php
@@ -1,5 +1,25 @@
 <?php
-//function which returns an array
+class myIterable implements Iterator {
+  private $items = [];
+  private $pointer = 0;
+
+  public function __construct($items){
+    $this->items = array_values($items);
+  }
+}
+
+//function to return the current element
+ public function current(){
+  return $this->items[$this->pointer];
+}
+
+
+
+
+
+
+
+/* //function which returns an array
 function getIterable():iterable {
   return ["a", "b", "c"];
 }
@@ -9,4 +29,4 @@ $myIterable = getIterable();
 //looping through the iterable
 foreach($myIterable as $item) {
   echo $item;
-}
+} */
