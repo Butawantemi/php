9c7568d8b41cacaf6bb6c041eda05335fdb05167
Butawantemi <japhetbuta@gmail.com>
2024-08-01 12:00:32 +0300

    create database

--- Database/createDb.php
+++ Database/createDb.php
@@ -10,3 +10,12 @@ try {
 }catch(mysqli_sql_exception $e){
     echo "" . $e->getMessage() . "";
 }
+
+//create database
+$sql = "CREATE DATABASE phpdb";
+if ($connect->query($sql) == TRUE)
+{
+    echo "Database created successfully";
+}else {
+    echo "Error creating database". $connect->error;
+}
