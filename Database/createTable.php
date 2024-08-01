539171d372ae4f8d836c6a1215e20a092c2c7e11
Butawantemi <japhetbuta@gmail.com>
2024-08-01 12:31:25 +0300

    create table

--- Database/createTable.php
+++ Database/createTable.php
@@ -10,3 +10,18 @@ $connect = mysqli_connect($server, $username, $password, $database);
 if (!$connect) {
     die("Connection failed" . mysqli_connect_error());
 }
+
+//sql to create table
+$sql = "CREATE TABLE MyGuests(
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+firstname VARCHAR(30) NOT NULL,
+lastname VARCHAR(30) NOT NULL,
+email VARCHAR(50),
+reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+)";
+
+if(mysqli_query($connect, $sql) == TRUE) {
+    echo "Table was succesfully created";
+}else {
+    echo "Creating table Error" . mysqli_error($connect);
+}
