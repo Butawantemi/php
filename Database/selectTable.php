848026f41d05c2d2e927d77e0a32e18163382c28
Butawantemi <japhetbuta@gmail.com>
2024-08-02 20:20:08 +0300

    select table

---
+++ Database/selectTable.php
@@ -0,0 +1,18 @@
+<?php
+$server = "127.0.0.1";
+$username = "root";
+$password = "Mp@ng01211";
+$database = "phpdb";
+
+
+$connect = mysqli_connect($server, $username, $password, $database);
+
+if (!$connect) {
+    die("Connection failed" . mysqli_connect_error());
+}
+
+//sql to select data from table.
+$sql = "SELECT * FROM MyGuests";
+$result = mysqli_query($connect, $sql);
+
+//return selected data
