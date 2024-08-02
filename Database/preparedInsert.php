2222ef7cb416bbeca0a177a3f05957c4900ace4d
Butawantemi <japhetbuta@gmail.com>
2024-08-02 19:49:56 +0300

    miltiple data use prepare

---
+++ Database/preparedInsert.php
@@ -0,0 +1,15 @@
+<?php
+$server = "127.0.0.1";
+$username = "root";
+$password = "Mp@ng01211";
+$database = "phpdb";
+
+
+$connect = mysqli_connect($server, $username, $password, $database);
+
+if (!$connect) {
+    die("Connection failed" . mysqli_connect_error());
+}
+
+//prepare and bind
+$stmt = $connect->prepare("INSERT INTO MyGuests(firstname, lastname, email) VALUES(?, ?, ?)");
