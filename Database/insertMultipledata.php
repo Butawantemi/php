5568812204b95b5427cd9aa67ea279fbedf6c979
Butawantemi <japhetbuta@gmail.com>
2024-08-02 19:26:54 +0300

    miltiple data

--- Database/insertMultipledata.php
+++ Database/insertMultipledata.php
@@ -11,6 +11,12 @@ if (!$connect) {
     die("Connection failed" . mysqli_connect_error());
 }
 
-//sql to insert data to table.
+//sql to insert multiple data to table.
 $sql = "INSERT INTO MyGuests(firstname, lastname, email)
-VALUES('Japhet', 'Paul', 'japhetbuta@gmail.com')";
+VALUES('Jackson', 'Buta', 'jackbuta@gmail.com')";
+
+$sql = "INSERT INTO MyGuests(firstname, lastname, email)
+VALUES('Jackline', 'Paul', 'jacklinep@gmail.com')";
+
+$sql = "INSERT INTO MyGuests(firstname, lastname, email)
+VALUES('Josephat', 'Wante', 'josebuta@gmail.com')";
