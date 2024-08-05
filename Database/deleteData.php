3a6ae0beb1d8607fd70ba121068e862948f4d767
Butawantemi <japhetbuta@gmail.com>
2024-08-05 18:40:43 +0300

    update data

--- Database/deleteData.php
+++ Database/deleteData.php
@@ -14,7 +14,7 @@ if (!$connect) {
 //delete data
 $sql = "DELETE FROM MyGuests WHERE id = 2";
 
-if (mysqli_query($connect, $sql)) {
+if (mysqli_query($connect, $sql) == TRUE) {
     echo "Record was deleted successfully";
 }else{
     echo "Error deleting record:" . mysqli_error($connect);
--- Database/updateData.php
+++ Database/updateData.php
@@ -10,3 +10,12 @@ $connect = mysqli_connect($server, $username, $password, $database);
 if (!$connect) {
     die("Connection failed" . mysqli_connect_error());
 }
+
+//update data
+$sql = "UPDATE MyGuests SET lastname  = 'Paul' WHERE id = 1";
+
+if (mysqli_query($connect, $sql) == TRUE){
+    echo "Record was updated successfully";
+}else {
+    echo "Error updating record:" . mysqli_error($connect);
+}
