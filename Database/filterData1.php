4fd6c888e0d7f76752384d62876d21e03ee09468
Butawantemi <japhetbuta@gmail.com>
2024-08-05 18:22:49 +0300

    order by

--- Database/filterData1.php
+++ Database/filterData1.php
@@ -10,3 +10,12 @@ $connect = mysqli_connect($server, $username, $password, $database);
 if (!$connect) {
     die("Connection failed" . mysqli_connect_error());
 }
+
+//order by
+$sql = "SELECT id, firstname, lastname FROM MyGuests ORDER BY firstname";
+$result = mysqli_query($connect, $sql);
+
+//return selected data
+if (mysqli_num_rows($result) > 0) {
+    while
+}
