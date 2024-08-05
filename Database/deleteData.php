12f28285dd0471f4e64b0eecdb3284e236612b60
Butawantemi <japhetbuta@gmail.com>
2024-08-05 18:29:33 +0300

    delete data

---
+++ Database/deleteData.php
@@ -0,0 +1,14 @@
+<?php
+$server = "127.0.0.1";
+$username = "root";
+$password = "Mp@ng01211";
+$database = "phpdb";
+
+
+$connect = mysqli_connect($server, $username, $password, $database);
+
+if (!$connect) {
+    die("Connection failed" . mysqli_connect_error());
+}
+
+//delete data
