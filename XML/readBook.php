fcc6f1c212f28edaa9b02148053275e0f3adef87
Butawantemi <japhetbuta@gmail.com>
2024-08-06 18:59:37 +0300

    xml

--- XML/readBook.php
+++ XML/readBook.php
@@ -1,4 +1,8 @@
 <?php
-$xml = simplexml_load_file("book.xml") or die("Error: Cannot create object");
-echo $xml->book[0]->title . "\n";
-echo $xml->book[1]->title . "\n";
+$xml = simplexml_load_file"book.xml") or die("Error: Cannot create object");
+foreach($xml->children() as $book){
+    echo $book->title . ", ";
+    echo $book->author . ", ";
+    echo $book->year . ", ";
+    echo $book->price . ", " . "\n";
+}
